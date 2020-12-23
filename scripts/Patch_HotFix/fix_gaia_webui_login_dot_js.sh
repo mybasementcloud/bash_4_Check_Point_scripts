072974b8aceb237530b16fffe8038536d8432ccb
Eric Beasley <mybasementcloud@gmail.com>
2020-12-22 22:48:41 -0600

    UPDATED 2020-12-22-2245CDT v05.02.00
    UPDATED 2020-12-22-2245CDT v05.02.00
    Minor updates and corrections, new MDM script for re-assigning global-assignments

--- scripts/Patch_HotFix/fix_gaia_webui_login_dot_js.sh
+++ scripts/Patch_HotFix/fix_gaia_webui_login_dot_js.sh
@@ -13,10 +13,10 @@
 # AUTHORIZE RESALE, LEASE, OR CHARGE FOR UTILIZATION OF THESE SCRIPTS BY ANY THIRD PARTY.
 #
 #
-ScriptDate=2020-12-14
-ScriptVersion=05.01.00
+ScriptDate=2020-12-22
+ScriptVersion=05.02.00
 ScriptRevision=000
-TemplateVersion=05.01.00
+TemplateVersion=05.02.00
 TemplateLevel=006
 SubScriptsLevel=NA
 SubScriptsVersion=NA
