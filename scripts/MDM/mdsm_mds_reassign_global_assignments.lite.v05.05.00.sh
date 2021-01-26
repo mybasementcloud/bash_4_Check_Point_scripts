c4e37aa02bb277e7fc5e4a818dd87a315d43c4c2
Eric Beasley <mybasementcloud@gmail.com>
2021-01-25 21:12:51 -0600

    UPDATED 2021-01-25-1945CST v05.05.00.000
    UPDATED 2021-01-25-1945CST v05.05.00.000
    Modified some things for migrate|migrate_server exports and alias commands

---
+++ scripts/MDM/mdsm_mds_reassign_global_assignments.lite.v05.05.00.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# (C) 2016-2021 Eric James Beasley, @mybasementcloud, https://github.com/mybasementcloud/bash_4_Check_Point_scripts
+#
+# ALL SCRIPTS ARE PROVIDED AS IS WITHOUT EXPRESS OR IMPLIED WARRANTY OF FUNCTION OR POTENTIAL FOR 
+# DAMAGE Or ABUSE.  AUTHOR DOES NOT ACCEPT ANY RESPONSIBILITY FOR THE USE OF THESE SCRIPTS OR THE 
+# RESULTS OF USING THESE SCRIPTS.  USING THESE SCRIPTS STIPULATES A CLEAR UNDERSTANDING OF RESPECTIVE
+# TECHNOLOGIES AND UNDERLYING PROGRAMMING CONCEPTS AND STRUCTURES AND IMPLIES CORRECT IMPLEMENTATION
+# OF RESPECTIVE BASELINE TECHNOLOGIES FOR PLATFORM UTILIZING THE SCRIPTS.  THIRD PARTY LIMITATIONS
+# APPLY WITHIN THE SPECIFICS THEIR RESPECTIVE UTILIZATION AGREEMENTS AND LICENSES.  AUTHOR DOES NOT
+# AUTHORIZE RESALE, LEASE, OR CHARGE FOR UTILIZATION OF THESE SCRIPTS BY ANY THIRD PARTY.
+#
+#
+# Version created 2020-12-22 1900 hrs CDT
+#
+ScriptDate=2021-01-25
+ScriptVersion=05.05.00
+ScriptRevision=000
+TemplateVersion=05.05.00
+TemplateLevel=006
+SubScriptsLevel=010
+SubScriptsVersion=05.05.00
+#
+
+
+
+echo '"global-domains","dependent-domains"' > global-assignment.header.csv
+cat global-assignment.header.csv > global-assignment.csv
+
+mgmt_cli -r true --port 4434 show global-assignments details-level full -f json | $JQ '.objects[] | [ .["global-domain"]["name"], .["dependent-domain"]["name"] ] | @csv' -r > global-assignment.data.csv
+
+cat global-assignment.data.csv >> global-assignment.csv
+
+cat global-assignment.csv
+
+mgmt_cli -r true --port 4434 -f json assign-global-assignment --batch global-assignment.csv
+
