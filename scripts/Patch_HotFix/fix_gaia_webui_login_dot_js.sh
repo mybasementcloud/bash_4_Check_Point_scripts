c6f3094e866e1993f2304fff23d313f2c3108c46
Eric Beasley <mybasementcloud@gmail.com>
2021-04-16 13:30:46 -0500

    UPDATED 2021-04-09-1830CDT v05.09.00
    UPDATED 2021-04-09-1830CDT v05.09.00

--- scripts/Patch_HotFix/fix_gaia_webui_login_dot_js.sh
+++ scripts/Patch_HotFix/fix_gaia_webui_login_dot_js.sh
@@ -13,10 +13,10 @@
 # AUTHORIZE RESALE, LEASE, OR CHARGE FOR UTILIZATION OF THESE SCRIPTS BY ANY THIRD PARTY.
 #
 #
-ScriptDate=2021-03-23
-ScriptVersion=05.08.00
+ScriptDate=2021-04-09
+ScriptVersion=05.09.00
 ScriptRevision=000
-TemplateVersion=05.08.00
+TemplateVersion=05.09.00
 TemplateLevel=006
 SubScriptsLevel=NA
 SubScriptsVersion=NA
